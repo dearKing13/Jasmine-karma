9b1799a87ee736b3118f3826b27ce5e9fbe37856
Diego R <diego.reyes@srax.com>
2018-05-22 16:36:08 -0700

    one more time

--- tests/checkIfDivExists.js
+++ tests/checkIfDivExists.js
@@ -1,7 +1,17 @@
-describe('getDiv', function(){
-	var d = document.querySelector('.box');
-	it('Should exist', function(){
-	    expect(d.nodeName).toBe('DIV');
-	});
+describe('getDiv', function() {
+    var d = document.querySelector('.box');
+
+    it('Should exist', function() {
+        expect(d.nodeName).toBe('DIV');
+    });
 });
+
+describe('getDivBg', function() {
+    var d = document.querySelector('.box');
+
+    it('Should be teal', function() {
+        expect(d.style.backgroundColor).toBe('teal');
+    });
+});
+
 
