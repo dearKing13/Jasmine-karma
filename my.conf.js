50a80f73e99f36cda436bba144e6ff6799044456
Diego R <diego.reyes@srax.com>
2018-05-24 09:23:39 -0700

    d_Spider

--- my.conf.js
+++ my.conf.js
@@ -11,7 +11,12 @@ module.exports = function(config) {
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine','es6-shim'],
-
+    
+    // NEW PLUGIN SECTION by Spider
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-phantomjs-launcher')    
+    ],
 
     // list of files / patterns to load in the browser
     files: [
